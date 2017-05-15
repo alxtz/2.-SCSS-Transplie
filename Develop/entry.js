7aa6c171ce98be2cd4c445d4de1746a3fd211a48
Alex Tzeng <alxtz.tw@gmail.com>
2017-05-15 12:23:09 +0800

    Finish SCSS support

--- Develop/entry.js
+++ Develop/entry.js
@@ -1,10 +1,3 @@
-// JS Modules
-var littleModule = require('./js/little.js');
-
-console.log('1. Init');
-console.log('2.1 Config');
-littleModule.logLittle();
-
-// CSS as Modules
-var style_cssModule = require('./style.css');
-console.log('3. Add CSS');
+// SCSS as Modules
+var style_scssModule = require('./style.scss');
+console.log('Add SCSS');
--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,8 @@
 # Adding SCSS Workflow
+
+## Adding SCSS
+1. change style.css to style.scss
+2. require in entry.js
+3. add sass-loader to config, and change the file type query
+4. check if support file import
+5. add scss/panels.scss , import it
